bdbf2d0eae26f94dddd453da466b66234ef5927e
Mariam Melhem <mary.melhem3@gmail.com>
2019-12-08 20:23:51 +0100

    Finished lab2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,27 @@
 public class Calculator {
-    public double add (double a,double b){
-        return a + b;
+    public double add(double a, double b) {
+        return add2Numbers(a, b);
     }
+
+    private double add2Numbers(double x, double y) {
+        return x + y;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    
+    public int divide(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+            return 0;
+        }
+    }
+
+    public double[] getArrayFromValues(double a, double b){
+        return new double[] {a, b};
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,52 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.w3c.dom.ls.LSOutput;
 
 public class CalculatorTest {
-    @Test
-    public void givenTwoNumbersThenAddReturnsCorrectSum(){
+
+    @ParameterizedTest
+    @CsvSource({"2,3,5", "-5,5,0", "-2,-2,-4", "0,-3,-3"})
+    public void givenTwoNumbersThenAddReturnsCorrectSum(double input1, double input2, double expected){
         Calculator c = new Calculator();
-        double sum = c.add(5, 2);
-        Assertions.assertEquals(7, sum);
+        double sum = c.add(input1, input2);
+        Assertions.assertEquals(expected, sum);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2,3,-1", "-5,5,-10", "-2,-2,0", "0,-3,3"})
+    public void givenTwoNumbersThenSubtractReturnsCorrectSub(double input1, double input2, double expected){
+        Calculator c = new Calculator();
+        double subtract = c.subtract(input1, input2);
+        Assertions.assertEquals(expected, subtract);
+    }
+
+    @Test
+    public void givenSecondNumberIsZeroThenDivideDoesNotThrowException(){
+        Assertions.assertDoesNotThrow(() -> {
+            var c = new Calculator();
+            var result = c.divide(9, 0);
+            Assertions.assertEquals(0, result);
+        });
+    }
+
+    @Test
+    public void arrayLengthTest(){
+        var c = new Calculator();
+        var result = c.getArrayFromValues(2, 3);
+        Assertions.assertEquals(2, result.length);
+    }
+
+    @BeforeEach
+    public void initializeTest(){
+        System.out.println("This is the beginning of a test");
+    }
+
+    @AfterEach
+    public void cleanupTest(){
+        System.out.println("Finishing the test");
     }
 }
